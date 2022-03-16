3aa398cccd02c1491ac4aaf2141c9883f1b33b18
MajaQamp <98764757+MajaQamp@users.noreply.github.com>
2022-03-16 17:23:23 +0100

    Update Main.java

--- OOP3/Main.java
+++ OOP3/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     Dog newDog = new Dog("Labrador", "male", 0.5, true, "black");
     Cat newCat = new Cat("100 % authentic stray cat", "female", 3, true, "mjauuuuu", true);
-    SiameseCat newSiameseCat = new SiameseCat("Siamese", "male", 10, false, "mjau!!!", true, "long");
+    SiameseCat newSiameseCat = new SiameseCat("Siamese cat", "male", 10, false, "mjau!!!", true, "long");
 
 newDog.isBlack();
 newDog.isPuppy();
