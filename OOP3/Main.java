47707028b3a13db372c19e1c0d16bde10b3104a5
Maja Mujanovic <mujanovic.@maja@gmail.com>
2022-03-16 17:02:07 +0100

    Zadaca  Inheritance

---
+++ OOP3/Main.java
@@ -0,0 +1,24 @@
+package OOP.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    Dog newDog = new Dog("Labrador", "male", 0.5, true, "black");
+    Cat newCat = new Cat("100 % authentic stray cat", "female", 3, true, "mjauuuuu", true);
+    SiameseCat newSiameseCat = new SiameseCat("Siamese", "male", 10, false, "mjau!!!", true, "long");
+
+newDog.isBlack();
+newDog.isPuppy();
+newDog.hasBeenNeutered();
+        System.out.println("\n");
+
+newCat.saysMeow();
+        System.out.println("\n");
+
+newSiameseCat.longHair();
+newSiameseCat.scratchesAfterMoew();
+
+
+}
+}
---
+++ OOP3/SiameseCat.java
@@ -0,0 +1,41 @@
+package OOP.Inheritance;
+
+public class SiameseCat extends Cat {
+    public String hair;
+
+
+    protected SiameseCat(String breed, String sex, double age, boolean neutered, String meow, boolean scratch, String hair) {
+        super(breed, sex, age, neutered, meow, scratch);
+        this.setBreed(breed);
+        this.setSex(sex);
+        this.setAge(age);
+        this.setNeutered(neutered);
+        this.setMeow(meow);
+        this.setScratch(scratch);
+        this.hair=hair;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    void scratchesAfterMoew(){
+        if (this.meow=="mjau!!!"){
+            this.scratch=true;
+            System.out.println("Me unhappy.");
+    }else{
+            this.scratch=false;
+        }
+}
+    public void longHair(){
+        if(this.hair=="long"){
+        System.out.println("Siamese cats have long hair");
+        }
+
+
+    }
+}
